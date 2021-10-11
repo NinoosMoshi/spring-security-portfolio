e2ae4e155544eb1f7c6e09aa63ba3fa42a8b3d2c
NinoosMoshi <nalkaton@gmail.com>
2021-10-11 08:24:54 -0400

    Spring create UserRepositort and AuthoritiesRepository Closes #3

---
+++ backEnd/login-registration/src/main/java/com/ninos/repository/AuthoritiesRepository.java
@@ -0,0 +1,9 @@
+package com.ninos.repository;
+
+import com.ninos.model.Authorities;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthoritiesRepository extends JpaRepository<Authorities, Long> {
+}
