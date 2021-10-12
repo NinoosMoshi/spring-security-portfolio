5f149438364bd89ce1163ecfece22c58a6cd5046
NinoosMoshi <nalkaton@gmail.com>
2021-10-12 10:47:58 -0400

    Spring create JwtLogin class Closes #10

---
+++ backEnd/login-registration/src/main/java/com/ninos/jwt/JwtLogin.java
@@ -0,0 +1,16 @@
+package com.ninos.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtLogin {
+
+    private String email;
+    private String password;
+
+
+}
