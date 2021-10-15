5a02c46e03f0ac0a3bd549a4f7bef4174999a50b
NinoosMoshi <nalkaton@gmail.com>
2021-10-15 14:30:20 -0400

    Angular Login component, install bootstrap and fontawesome Closes #16

--- frontEnd/login-registration-app/src/app/app-routing.module.ts
+++ frontEnd/login-registration-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:SignInComponent},
+  {path:'',redirectTo:'/login',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
