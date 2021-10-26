8ef32948ab530c8ed4d43124b6538191d5382260
NinoosMoshi <nalkaton@gmail.com>
2021-10-26 08:48:44 -0400

    Angular Signup component Closes #17

--- frontEnd/login-registration-app/src/app/app-routing.module.ts
+++ frontEnd/login-registration-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'login', component:SignInComponent},
+  {path:'signup', component:SignUpComponent},
   {path:'',redirectTo:'/login',pathMatch:'full'}
 ];
 
